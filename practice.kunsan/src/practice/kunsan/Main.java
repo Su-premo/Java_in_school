8dd7f6befedbd32a960423fddc56e687ae8fb046
supreme <aj1234@hotmail.co.kr>
2021-06-05 14:19:44 +0900

    Practice1. convert celsius into fahrenheit

--- practice.kunsan/src/practice/kunsan/Main.java
+++ practice.kunsan/src/practice/kunsan/Main.java
@@ -1,9 +1,16 @@
 package practice.kunsan;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+      public static void main(String[] args) {
+           Scanner sc = new Scanner(System.in);
+           float celsius, fahrenheit;
+           System.out.print("Enter temperature in Celsius: ");
 
+           celsius = sc.nextFloat();
+           fahrenheit = 32 + (celsius * 9 / 5);
+           System.out.println(celsius +" ¨¬C is equal to " + fahrenheit + " ¨¬F");                                  
+     }
 }
