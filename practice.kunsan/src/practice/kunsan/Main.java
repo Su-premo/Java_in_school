8a2e604752ae72e43663e819c4b07114577267e8
supreme <aj1234@hotmail.co.kr>
2021-06-05 15:42:20 +0900

    Practice5. convert numbered grade into letter grade (**gotta check it out how to close Scanner class**)

--- practice.kunsan/src/practice/kunsan/Main.java
+++ practice.kunsan/src/practice/kunsan/Main.java
@@ -5,16 +5,19 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int line;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the number of rows you want to print: ");
-		line = input.nextInt();
-		
-		for (int i = 0; i<line; i++) {
-			for(int j = 0; j<=i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		System.out.println("Enter the numbered grade you received: ");
+		int score = input.nextInt();
+
+		if (score >= 90) {
+			System.out.println("your grade is A.");
+		} else if (score >= 80 && score < 90)
+			System.out.println("your grade is B.");
+		else if (score >= 70 && score < 80)
+			System.out.println("your grade is C.");
+		else if (score >= 60 && score < 70)
+			System.out.println("your grade is D.");
+		else if (score < 60)
+			System.out.println("your grade is F.");
 	}
 }
